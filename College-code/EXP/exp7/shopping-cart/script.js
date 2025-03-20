351e703b449235f0611991226fb9e0df17158942
1dhruvsingh <142042993+1dhruvsingh@users.noreply.github.com>
2025-03-20 12:39:56 +0530

    components replaced

--- College-code/EXP/exp7/shopping-cart/script.js
+++ College-code/EXP/exp7/shopping-cart/script.js
@@ -1,18 +1,37 @@
 var app = angular.module("shoppingApp", []);
+
 app.controller("cartController", function($scope) {
+    $scope.products = [
+        {name: 'Laptop', price: 50000, quantity: 0},
+        {name: 'Phone', price: 20000, quantity: 0},
+        {name: 'Headphones', price: 3000, quantity: 0},
+        {name: 'Smart Watch', price: 8000, quantity: 0},
+        {name: 'Camera', price: 45000, quantity: 0}
+    ];
+
     $scope.addToCart = function(product) {
         product.quantity++;
     };
+
     $scope.removeFromCart = function(product) {
         if (product.quantity > 0) {
             product.quantity--;
         }
     };
+
     $scope.getTotal = function() {
         let total = 0;
         angular.forEach($scope.products, function(product) {
             total += product.quantity * product.price;
         });
         return total;
+    };
+
+    $scope.getTotalItems = function() {
+        let count = 0;
+        angular.forEach($scope.products, function(product) {
+            count += product.quantity;
+        });
+        return count;
     };
 });
